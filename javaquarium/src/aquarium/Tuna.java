07b8bbdf097116e8101631caf820b4b874c7e6f9
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-01-29 23:44:27 +0100

    Add of the six species

---
+++ javaquarium/src/aquarium/Tuna.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aquarium;
+
+/**
+ *
+ * @author doyenm
+ */
+public class Tuna extends CarnivorousFish{
+
+    public Tuna(String name, Sex sex, int age) {
+        super(name, sex, age);
+    }
+    
+}
